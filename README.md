b5b61bc45dfb2a08d27c64aae1a0450a0eac2812
SBidaibek <sanzhar.bidaibek@gmail.com>
2019-11-20 22:51:28 +0800

    Header Vector3d.h properly defined

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mini-Ray-Tracer
 Self Proposed Project for COMP 2012H
+
+The current To-Do list is in the "Issues" tab.
+
+Rules:
+1) Do not commit without discussing first
+2) Do not use "using namespace std"
--- vector.h
+++ vector.h
@@ -1,8 +1,42 @@
-
-class vector3d {
+class Vector3d {
+private:
     double x, y, z;
+public:
+    ///constructors
+    Vector3d();
 
-    vector3d operator + (const vector3d &rhs);
-    double operator * (const vector3d &rhs);
-    vector3d operator ^ (const vector3d &rhs);
+    Vector3d(double x, double y, double z);
+
+    ///accessors
+    double getX(double x) const;
+
+    double getY(double y) const;
+
+    double getZ(double z) const;
+
+    double norm() const;
+
+    double operator[](int dim_id);
+
+    ///mutators
+    void setX(double x);
+
+    void setY(double y);
+
+    void setZ(double z);
+
+    ///operators
+    Vector3d operator -() const; ///negation
+
+    Vector3d operator + (const Vector3d &rhs) const; ///summation
+
+    Vector3d operator - (const Vector3d &rhs) const; ///difference
+
+    Vector3d operator * (double c) const; ///multiplication by a scalar
+
+    double operator * (const Vector3d &rhs) const; ///dot product
+
+    Vector3d operator ^ (const Vector3d &rhs) const; ///cross product
 };
+
+double getAngle(const Vector3d &v1, const Vector3d &v2); ///angle between two vectors in radians

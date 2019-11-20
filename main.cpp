fb9e2968bdc860659e47effb57491e6936989603
SBidaibek <sanzhar.bidaibek@gmail.com>
2019-11-20 19:28:44 +0300

    stb imported

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "external/stb_image_write.h"
+#include <vector>
+
+void gen_gradient() {
+    const int width = 500;
+    const int height = 300;
+    const int comp = 3;
+
+    std::vector <unsigned char> image(width * height * comp);
+
+    for (int i = 0; i < width; ++i) {
+        for (int j = 0; j < height; ++j) {
+            int cell = i * height * comp + j * comp;
+            image[cell + 0] = (float(i) / width) * 256;
+            image[cell + 1] = (float(j) / height) * 256;
+            image[cell + 2] = image[cell + 0] + image[cell + 1];
+        }
+    }
+
+    stbi_write_png("img.png", width, height, comp, image.data(), height * comp);
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    gen_gradient();
     return 0;
 }
